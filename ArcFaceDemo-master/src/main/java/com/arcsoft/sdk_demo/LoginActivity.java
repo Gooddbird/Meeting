bb276c9360a4df9715554c051ee54fc6716120c8
HD <Hd@gmail.com>
2018-12-07 15:29:24 +0800

    First commit

---
+++ ArcFaceDemo-master/src/main/java/com/arcsoft/sdk_demo/LoginActivity.java
@@ -0,0 +1,39 @@
+package com.arcsoft.sdk_demo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText etUserName;
+    private EditText etUserPassword;
+    private Button login;
+    private EditText userName;
+    private EditText userPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        init();
+    }
+    private void init(){
+        userName = (EditText) findViewById(R.id.et_userName);
+        userPassword = (EditText) findViewById(R.id.et_password);
+        ImageView unameClear = (ImageView) findViewById(R.id.iv_unameClear);
+        ImageView pwdClear = (ImageView) findViewById(R.id.iv_pwdClear);
+        login=(Button)findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
